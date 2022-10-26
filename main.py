603449816255fb16480f4b13d42bc04d95512c8a
Levi Matheus <levi.matheus@outlook.com>
2022-10-26 12:56:21 -0300

    Commit inicial do orientação a objetos
    - Herança simples
    - Métodos de classe
    - Atributos de classe
    - Métodos estáticos, privados, públicos

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bola_jabulani = Bola('Jabulani','Copa Oficial','Branca','Normal')
 #print(f"{garfo_normal.nome} é do tipo {garfo_normal.tipo}, cor {garfo_normal.cor} e tamanho {garfo_normal.tamanho}")
 #print(f"{bola_jabulani.nome} é do tipo {bola_jabulani.tipo}, cor {bola_jabulani.cor} e tamanho {bola_jabulani.tamanho}")
 
-#CHAMANDO METODO ESPECIFICO DA CLASSE FILHA PELA INSTANCIA
+#CHAMANDO METODO ESPECIFICO DA CLASSE FILHA PELA INSTANCIA 
 garfo_normal.pegar_talher()
 
 #CHAMANDO METODO DA CLASSE PAI QUE USA ATRIBUTO DE CLASSE
